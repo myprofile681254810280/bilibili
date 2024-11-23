5edaba1e1217bc72f1ef31a364d2030cbe632028
my <my@my.my>
2024-11-23 12:53:46 +0800

    bilibili

--- play.sh
+++ play.sh
@@ -1,11 +1,20 @@
-mkdir /dev/shm/bilibili
-cp ./cookies.txt /dev/shm/bilibili/
-cd /dev/shm/bilibili
-rm *.mp4
+p0=/dev/shm/bilibili #别带空格
+#/dev/shm/bilibili
+#cp ./cookies.txt /dev/shm/bilibili/
+#cd /dev/shm/bilibili
+#rm *.mp4
 #yt-dlp --cookies ./cookies.txt "$@"
 #用--get-url可以抽取url，但是有音频和视频，需要合并，并且mpv直接打开视频会有403，  和直播比起来，中间的麻烦还挺多的，似乎并不能那么简单的交给mpv
 
 #开一个chromium profile，专门给ytdlp读取登录cookie，而不用担心主浏览器cookie泄漏
+
+mkdir $p0
+cd $p0
+
 yt-dlp "$@" --cookies-from-browser chrome:~/cache/chromium-ytdlp
 
-mpv *.mp4
+#下载完后手动开，不删旧文件
+dolphin $p0
+
+
+#mpv *.mp4
